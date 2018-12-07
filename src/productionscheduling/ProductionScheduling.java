95333aa72253f7a09bdb22f6029fc36828cab2a8
kayakIYD <ficupaolo@gmail.com>
2018-12-07 15:58:57 +0100

    Initial commit on branch master - add minimal working code

---
+++ src/productionscheduling/ProductionScheduling.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package productionscheduling;
+
+import gui.MainWindow;
+
+/**
+ *
+ * @author PF
+ */
+public class ProductionScheduling {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainWindow mw;
+        mw = new MainWindow();
+        mw.setVisible(true);
+    }
+    
+}
